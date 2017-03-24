7dca14fa7ac6c5fd96ed1dc950b0357a5e1a3d64
L337Lobster <jazzycool9@gmail.com>
2017-03-24 09:39:05 -0400

    Blob implementation in user
    password will be stored as a Blob in the database

--- model/xyz/jhofmann1/cs320/model/main/User.java
+++ model/xyz/jhofmann1/cs320/model/main/User.java
@@ -2,6 +2,10 @@ package xyz.jhofmann1.cs320.model.main;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import javax.sql.rowset.serial.SerialException;
 
 /**
  * 
@@ -17,6 +21,7 @@ public class User {
 	 */
 	private String username;
 	private byte[] password, salt;
+	private Blob passwordStore;
 	
 	public User() throws NoSuchAlgorithmException, InvalidKeySpecException
 	{
@@ -24,6 +29,12 @@ public class User {
 		PasswordEncryptionService encrypt = new PasswordEncryptionService();
 		salt = encrypt.generateSalt();
 		password = encrypt.getEncryptedPassword("password", salt);
+		try {
+			passwordStore = getBlobPassword(password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public User(String username, String password) throws NoSuchAlgorithmException, InvalidKeySpecException
@@ -32,13 +43,47 @@ public class User {
 		PasswordEncryptionService encrypt = new PasswordEncryptionService();
 		salt = encrypt.generateSalt();
 		this.password = encrypt.getEncryptedPassword(password, salt);
+		try {
+			passwordStore = getBlobPassword(this.password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public User(String username, byte[] password)
 	{
 		this.username = username;
 		this.password = password;
+		try {
+			passwordStore = getBlobPassword(password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public User(String username, Blob password)
+	{
+		this.username = username;
+		passwordStore = password;
+		try {
+			this.password = getBytePassword(password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	private byte[] getBytePassword(Blob pass) throws SQLException
+	{
+		int blobLength = (int) pass.length();  
+		byte[] blobAsBytes = pass.getBytes(1, blobLength);
+		return blobAsBytes;
+	}
+	private Blob getBlobPassword(byte[] pass) throws SerialException, SQLException
+	{
+		Blob blob = new javax.sql.rowset.serial.SerialBlob(pass);
+		return blob;
+	}
 	/**
 	 * @return the username
 	 */
