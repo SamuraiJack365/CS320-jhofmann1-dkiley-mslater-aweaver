fc72a207e08311c661040ee4243916a1c09f923b
L337Lobster <jazzycool9@gmail.com>
2017-04-01 17:03:50 -0400

    Added multiple test users (i.e. fake database) in LoginController

--- servlet/xyz/jhofmann1/cs320/servlet/main/LoginServlet.java
+++ servlet/xyz/jhofmann1/cs320/servlet/main/LoginServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import xyz.jhofmann1.cs320.controller.main.LoginController;
-import xyz.jhofmann1.cs320.controller.main.PasswordEncryptionService;
 
 /**
  * @author Jackson
