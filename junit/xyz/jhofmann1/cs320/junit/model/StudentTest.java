fb06e7ad094f2d78e1ecdb0a53740aa7f536c308
AlaskaJay <thatthinker@gmail.com>
2017-04-05 09:45:55 -0400

    Student Test Finished
    We need to figure out how the database and the model exchange the
    videos, pictures, links, and what have you

--- junit/xyz/jhofmann1/cs320/junit/model/StudentTest.java
+++ junit/xyz/jhofmann1/cs320/junit/model/StudentTest.java
@@ -21,14 +21,50 @@ public class StudentTest {
 	
 	@Before
 	public void setUp() throws NoSuchAlgorithmException, InvalidKeySpecException{
+		//studentID, accolades, majors, minors, activities, officer, studentIDNum, 
+		//GPA, displayGPA, reviewed, approvalState
 		majors = new int[3]; majors[0] = 1; majors[2] = 7;
 		minors = new int[3]; minors[0] = 8;
 		activities = new int[4];
+		activities[0] = 12;
+		activities[1] = 16;
+		
+		officer = new int[4];
+		officer[1] = 3;
 		
 		fields = new ArrayList<Object>();
 		fields.add("903589175");
 		fields.add("GOOD STUDENT");
+		fields.add(majors);
+		fields.add(minors);
+		fields.add(activities);
+		fields.add(officer);
+		fields.add(16);
+		fields.add(2.54);
+		fields.add(true);
+		fields.add(false);
+		fields.add(1);
+		fields.add("jsmith@ycp.edu");
+		fields.add("Joe");
+		fields.add("Smith");
+		
+		System.out.println(fields);
 		
 		model = new Student("Username", "Password", fields);
+	}
+	
+	public void testGetters() {
+		assertEquals("903589175",model.getStudentID());
+		assertEquals("GOOD STUDENT",model.getAccolades());
+		assertEquals(majors,model.getMajors());
+		assertEquals(minors,model.getMinors());
+		assertEquals(activities,model.getActivities());
+		assertEquals(officer,model.getOfficer());
+		assertEquals(16,model.getStudentIDNum());
+		assertEquals(true,model.isDisplayGPA());
+		assertEquals(false,model.isReviewed());
+		assertEquals("jsmith@ycp.edu",model.getEmail());
+		assertEquals("Joe",model.getStudentFirstName());
+		assertEquals("Smith",model.getStudentLastName());
 	}
 }
